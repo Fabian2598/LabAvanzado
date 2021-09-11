b7a8f5012bad2bf4860bcd2f80ecb287aae99eba
Laptop <jafania@ciencias.unam.mx>
2021-09-11 18:05:37 -0500

    Tarea5

--- Tarea5/Prob3.C
+++ Tarea5/Prob3.C
@@ -1,5 +1,6 @@
 #include <iostream>
 
+//Problema 3 de la tarea 5.
 
 void Prob3(){
     TFile *file = TFile::Open("trig_run009776_00692.root");
@@ -8,7 +9,6 @@ void Prob3(){
     
     Int_t entradas = arbol->GetEntries();
 
-    //Trabajamos nada más con pulsos de dos bordes.
     Int_t HitsMax = 2000; //Suponemos que a lo más tenemos 2,000 hits (Un PMT se puede disparar más de una vez)
     ULong64_t Hit2Edge;
     ULong64_t Hit4Edge;
@@ -32,27 +32,46 @@ void Prob3(){
     arbol->SetBranchAddress("trig.hitTime_4Edge",hitTime_4Edge);
     arbol->SetBranchAddress("trig.flags_4Edge",flags_4Edge);
 
-    float Freq2Edge[entradas];
-    float Freq4Edge[entradas];
-    for(int i=0; i<entradas; i++){
-        arbol->GetEntry(i);
-        Freq2Edge[i] = Hit2Edge/(1.5); //Suponemos que cada evento dura 1.5 microsegundos. Ponemos los resultados en GhZ
-        Freq4Edge[i] = Hit4Edge/(1.5);
+    double Freq2Edge[1200]; 
+    double Freq4Edge[1200];
+    double Freq2y4Edge[1200];
+    for(int i = 0; i<1200; i++){
+        Freq2Edge[i] = 0;
+        Freq4Edge[i] = 0;
+        Freq2y4Edge[i] = 0;
     }
 
+    for(int i=0; i<entradas; i++){
+        arbol->GetEntry(i);
+        int j = Hit2Edge;
+        int k = Hit4Edge;
+        
+        Freq2Edge[j] += Hit2Edge; //Si se cuenta un número Hit2Edge en algún evento, entonces sumamos Hit2Edge a la entrada
+        //enumerada por Hit2Edge a este arreglo. 
+        Freq4Edge[k] += Hit4Edge;
+        Freq2y4Edge[j+k] += j+k;
+
+    }
+for(int i = 0; i<1200; i++){
+        Freq2Edge[i] /= 120; //El tiempo total del evento fueron dos minutos, por lo que dividimos entre 1200
+        Freq4Edge[i] /= 120;
+        Freq2y4Edge[i] /= 120;
+    }
     
 
-    //Podemos graficar primero en todo el rango de valores con este evento y nos daremos cuenta de que 
-    //entre 0 y 3000 TDC hay muchos hits que se ven correlacionados. 
-    auto cvEv1 = new TCanvas("cb","hist1",1100,800);
-    TH1F *h1 = new TH1F("h1","Numero de evento vs. frecuencia de hits de 2 edges; No. evento; Frecuencia [MHz]",entradas-1,1,entradas); 
-
+     auto cvEv1 = new TCanvas("cb","hist1",1100,800);
+     TH1D *h1 = new TH1D("h1","Numero de hits de 2 edges vs. frecuencia; No. de Hits; Frecuencia [Hz]",400,1,401); 
+     
     for(int i=0; i<entradas; i++){
-        h1->SetBinContent(i+1,Freq2Edge[i]);
+        arbol->GetEntry(i);
+        //Graficamos hasta un rango máximo de 400 hits, ya que como se verá en la gráfica, la curva decae muy rápido.
+        if (Hit2Edge<=400){
+            int j = Hit2Edge;
+            h1->SetBinContent(Hit2Edge,Freq2Edge[j]);
+        }
     } 
 
-    //h1->SetLineColor(1);
-    //h1->SetFillColor(7);
+    h1->SetFillColor(2);
     h1->SetBarWidth(1);
     h1->SetStats(0);
 
@@ -60,33 +79,44 @@ void Prob3(){
     cvEv1->cd(1);
     cvEv1->SaveAs("Prob32Edge.pdf");
 
+
+
     auto cvEv2 = new TCanvas("cb2","hist2",1100,800);
-    TH1F *h2 = new TH1F("h2","Numero de evento vs. frecuencia de hits de 4 edges; No. evento; Frecuencia [MHz]",entradas-1,1,entradas); 
+    TH1F *h2 = new TH1F("h2","Numero de hits de 4 edges vs. frecuencia; No. de Hits; Frecuencia [Hz]",400-1,1,401); 
     h2->SetStats(0);
 
     for(int i=0; i<entradas; i++){
-        h2->SetBinContent(i+1,Freq4Edge[i]);
+         arbol->GetEntry(i);
+        if (Hit4Edge<=400){
+            int j = Hit4Edge;
+            h2->SetBinContent(Hit4Edge,Freq4Edge[j]);
+        }
     } 
 
-    //h2->SetLineColor(1);
-    //h2->SetFillColor(2);
+    h2->SetFillColor(2);
     h2->SetBarWidth(1);
     h2->Draw();
     cvEv2->cd(1);
     cvEv2->SaveAs("Prob34Edge.pdf");
-    
-    auto cv = new TCanvas("cb3","hist3",1100,800);
-    //gStyle->SetOptTitle(0);
-    TH1F *h3 = new TH1F("h3","Numero de evento vs. frecuencia de hits de 2 y 4 edges; No. evento; Frecuencia [MHz]",entradas-1,1,entradas); 
-   
+
+    auto cvEv3 = new TCanvas("cb3","hist3",1100,800);
+    TH1F *h3 = new TH1F("h3","Numero de hits de 2 y 4 edges vs. frecuencia; No. de Hits; Frecuencia [Hz]",400-1,1,401); 
     h3->SetStats(0);
     for(int i=0; i<entradas; i++){
-        h3->SetBinContent(i+1,Freq2Edge[i]+Freq4Edge[i]);
+         arbol->GetEntry(i);
+        if ((Hit4Edge+Hit2Edge)<=400){
+            int j = Hit4Edge + Hit2Edge;
+            h3->SetBinContent(j,Freq2y4Edge[j]);
+        }
     } 
+
+    h3->SetFillColor(2);
     h3->SetBarWidth(1);
     h3->Draw();
-    cv->cd(1);
-    cv->SaveAs("Prob32y4Edge.pdf");
+    cvEv3->cd(1);
+    cvEv3->SaveAs("Prob32y4Edge.pdf");
+    
+    //Ahora consideremos la combinación de ambos pulsos.
 
 
 }
