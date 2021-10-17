af758fb60ceaf2017ea5430137fc00f407ef6042
Laptop <jafania@ciencias.unam.mx>
2021-10-16 20:46:11 -0500

    Tarea9

--- Tarea9/Prob3.C
+++ Tarea9/Prob3.C
@@ -16,17 +16,35 @@ void Prob3(){
 
     //Energía reconstruida red neuronal
     auto cv = new TCanvas("cb","hist",750,550);
-    TH2D *h = new TH2D("Energ#acute{i}a #gamma", "Energ#acute{i}a simulada vs. energ#acute{i}a reconstruida, red neuronal; log_{10}(E_{sim}) [log_{10}(GeV)]; log_{10}(E_{rec}) [log_{10}(GeV)]",100,0,6,100,2,5); 
+    TH2D *h = new TH2D("Energ#acute{i}a #gamma", "Energ#acute{i}a simulada vs. energ#acute{i}a reconstruida, red neuronal; log_{10}(E_{sim}) [GeV]; log_{10}(E_{rec}) [GeV]",100,0,6,100,2,5); 
     for(int i=0; i<entradas_g; i++){
         arbolg->GetEntry(i);
-        h->Fill(log10(Esim_G),logERN_G);
+        h->Fill(log10(Esim_G),logERN_G);//Graficamos en escala logarítmica.
     }
     h->SetContour(50);
-      // TGaxis *axis4 = new TGaxis(-7,-0.8,-7,0.8,1,10000,50510,"G");
-   //axis4->SetName("axis4");
-   //axis4->Draw();
-    h->Draw("colz");
 
+    TAxis *a = h->GetXaxis();
+    a->SetTitle("E_{sim} [GeV]");
+    a->SetNdivisions(7,0,0);
+    a->ChangeLabel(2,-1,-1,-1,-1,-1,"10^{1}");
+    a->ChangeLabel(3,-1,-1,-1,-1,-1,"10^{2}");
+    a->ChangeLabel(4,-1,-1,-1,-1,-1,"10^{3}");
+    a->ChangeLabel(5,-1,-1,-1,-1,-1,"10^{4}");
+    a->ChangeLabel(6,-1,-1,-1,-1,-1,"10^{5}");
+    a->ChangeLabel(7,-1,-1,-1,-1,-1,"10^{6}");
+
+
+    TAxis *b = h->GetYaxis();
+    b->SetTitle("E_{rec} [GeV]");
+    b->SetNdivisions(4,0,0);
+    b->ChangeLabel(1,-1,-1,-1,-1,-1,"10^{2}");
+    b->ChangeLabel(2,-1,-1,-1,-1,-1,"10^{3}");
+    b->ChangeLabel(3,-1,-1,-1,-1,-1,"10^{4}");
+    b->ChangeLabel(4,-1,-1,-1,-1,-1,"10^{5}");
+
+    h->Draw("colz");
+    a->Draw("Same");
+    b->Draw("Same");
 
   //Ajuste lineal
    TF1 *f1 = new TF1("f1","[0]+[1]*x",0,6);
@@ -62,6 +80,29 @@ void Prob3(){
         h2->Fill(log10(Esim_G),logEGP_G);
     }
     h2->SetContour(50);
+    a = h2->GetXaxis();
+    a->SetTitle("E_{sim} [GeV]");
+    a->SetNdivisions(7,0,0);
+    a->ChangeLabel(2,-1,-1,-1,-1,-1,"10^{1}");
+    a->ChangeLabel(3,-1,-1,-1,-1,-1,"10^{2}");
+    a->ChangeLabel(4,-1,-1,-1,-1,-1,"10^{3}");
+    a->ChangeLabel(5,-1,-1,-1,-1,-1,"10^{4}");
+    a->ChangeLabel(6,-1,-1,-1,-1,-1,"10^{5}");
+    a->ChangeLabel(7,-1,-1,-1,-1,-1,"10^{6}");
+
+
+    b = h2->GetYaxis();
+    b->SetTitle("E_{rec} [GeV]");
+    b->SetNdivisions(7,0,0);
+    b->ChangeLabel(2,-1,-1,-1,-1,-1,"10^{1}");
+    b->ChangeLabel(3,-1,-1,-1,-1,-1,"10^{2}");
+    b->ChangeLabel(4,-1,-1,-1,-1,-1,"10^{3}");
+    b->ChangeLabel(5,-1,-1,-1,-1,-1,"10^{4}");
+    b->ChangeLabel(6,-1,-1,-1,-1,-1,"10^{5}");
+    b->ChangeLabel(7,-1,-1,-1,-1,-1,"10^{6}");
+
+
+
     h2->Draw("colz");
 
     gPad->Update();
@@ -84,8 +125,8 @@ void Prob3(){
    std::cout << "b = " << fitResult2->GetParameter(1) << std::endl;
    std::cout << "" << std::endl;
    f2->Draw("Same");
-    cv2->cd(1);
-    cv2->SaveAs("Prob3B.pdf");
+   cv2->cd(1);
+   cv2->SaveAs("Prob3B.pdf");
 
 
 }
