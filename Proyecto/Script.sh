eb456c77a5b0f8ea1f4f85f8ac6caa863ab55a10
Laptop <jafania@ciencias.unam.mx>
2021-11-03 15:00:15 -0600

    Script.sh

---
+++ Proyecto/Script.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+root -l <<-EOF
+.x P5.C
+../luna_protonlhe/tabla_0.dat
+0
+EOF
+root -l <<-EOF
+.x P5.C
+../luna_protonlhe/tabla_1.dat
+1
+EOF
+root -l <<-EOF
+.x P5.C
+../luna_protonlhe/tabla_2.dat
+2
+EOF
+root -l <<-EOF
+.x P5.C
+../luna_protonlhe/tabla_3.dat
+3
+EOF
+root -l <<-EOF
+.x P5.C
+../luna_protonlhe/tabla_4.dat
+4
+EOF
+root -l <<-EOF
+.x P5.C
+../luna_protonlhe/tabla_5.dat
+5
+EOF
+root -l <<-EOF
+.x P5.C
+../luna_protonlhe/tabla_6.dat
+6
+EOF
+root -l <<-EOF
+.x P5.C
+../luna_protonlhe/tabla_7.dat
+7
+EOF
+root -l <<-EOF
+.x P5.C
+../luna_protonlhe/tabla_8.dat
+8
+EOF
+root -l <<-EOF
+.x P5.C
+../luna_protonlhe/tabla_9.dat
+9
+EOF
